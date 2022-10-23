7e0eb982a53eb1ffcc54c02a5c8790adc5883a77
Rtem76 <110050771+Rtem76@users.noreply.github.com>
2022-10-24 01:10:24 +0200

    I found a clear way, for such a noob as me, to solve most of my problems with DICT and Classes. Some of primitive mechanics of my game are working well. But look's like it will take more time to write code... Simple Test's of game mechanics are in attribyte.py

--- skills.py
+++ skills.py
@@ -1,60 +1,102 @@
-#from dice import dice
+import copy
 
-class Skills:
-    def __init__(self, skill_name: int, proficiency: int, mastered = None):
-        self.skill_name = skill_name
-        self.proficiency = proficiency
-        self.mastered = mastered
-        if self.proficiency >= self.mastered
-            self.mastered = self.proficiency
+# skills dictionary => skill aka key: [value, mastering, "description"]
+
+skills_initial = {
+    "Accounting": [10, None],
+    "Anthropology": [1, None],
+    "Archaeology": [1, None],
+    "Art": [5, None],
+    "Astronomy": [1, None],
+    "Bargain": [5, None],
+    "Biology": [1, None],
+    "Chemistry": [1, None],
+    "Climb": [40, None],
+    "Conceal": [15, None],
+    "Craft": [5, None],
+    "Credit Rating": [15, None],
+    "Disguise": [1, None],
+    "Dodge": [10, None],
+    "Drive Automobile": [20, None],
+    "Electrical Repair": [10, None],
+    "Fast Talk": [5, None],
+    "First Aid": [30, None],
+    "Fist/Punch": [50, None],
+    "Geology": [1, None],
+    "Grapple": [25, None],
+    "Handgun": [20, None],
+    "Head Butt": [10, None],
+    "Hide": [10, None],
+    "History": [20, None],
+    "Jump": [25, None],
+    "Kick": [25, None],
+    "Law": [10, None],
+    "Library Use": [25, None],
+    "Listen": [25, None],
+    "Locksmith": [1, None],
+    "Machine Gun": [15, None],
+    "Martial Arts": [1, None],
+    "Mechanical Repair": [20, None],
+    "Medicine": [5, None],
+    "Natural History": [10, None],
+    "Navigate": [10, None],
+    "Occult": [5, None],
+    "Operate Heavy Machine": [1, None],
+    "Other Language": [1, None],
+    "Own Language": [5, None],
+    "Persuade": [15, None],
+    "Pharmacy": [1, None],
+    "Photography": [10, None],
+    "Physics": [1, None],
+    "Pilot": [1, None],
+    "Psychoanalysis": [1, None],
+    "Psychology": [5, None],
+    "Ride": [5, None],
+    "Rifle": [25, None],
+    "Shotgun": [30, None],
+    "Sneak": [10, None],
+    "Spot Hidden": [25, None],
+    "Submachine Gun": [15, None],
+    "Swim": [25, None],
+    "Throw": [25, None],
+    "Track": [10, None],
+    "Melee Weapon": [25, None],
+}
+#p1_skills = copy.deepcopy(skills_initial)
+#p2_skills = copy.deepcopy(skills_initial)
+
+class Skills():
+    def __init__(self, skills: dict):
+        self.skills = skills
 
     def __str__(self):
         result = str(self)
         print(f"{result}")
 
+    def find_skill_value(self, skill_name):
+        return self.skills[skill_name][0]
 
+    def raise_skill(self, skill_name):
+        result_of_dice = dice(100)
+        current = self.find_skill_value(skill_name)
+        if result_of_dice > current:
+            if result_of_dice >= current + 10:
+                current += 10
+            else:
+                current = result_of_dice
+            print(f"Congratulation's your skill of {skill_name} rises to {current}")
+            self.skills[skill_name][0] = current
+def main():
+    mario = Skills(copy.deepcopy(skills_initial))
+    any = Skills(copy.deepcopy(skills_initial))
+    any.raise_skill("Accounting")
+    mario.skills["Melee Weapon"][0] = 40
+    mario.raise_skill("Melee Weapon")
+    print(f' Now Any have {any.find_skill_value("Accounting")} point in Acoounting skill')
+    print(f' Now Mario have {mario.find_skill_value("Accounting")} point in Acoounting skill')
+    print(f' Now Any have {any.find_skill_value("Melee Weapon")} point in Melee weapon skill')
+    print(f' Now Mario have {mario.find_skill_value("Melee Weapon")} point in Melee weapon skill')
+    # skills dictionary => skill aka key: [value, mastering, "description"]
 
-    def raise_skill(self, stat):
-        if stat == "accounting":
-            result_of_dice = dice(100)
-            if result_of_dice > self.accounting:
-                if result_of_dice >= self.accounting + 10:
-                    self.accounting +=10
-                else:
-                    self.accounting = dice
-                print(f"Congratulation's your skill of accounting rises to {self.accounting}")
-
-        elif stat == "athletics":
-            result_of_dice = dice(100)
-            if result_of_dice > self.athletics:
-                if result_of_dice >= self.athletics + 10:
-                    self.athletics +=10
-                else:
-                    self.athletics = dice
-            elif stat == "one_handed":
-                result_of_dice = dice(100)
-                if result_of_dice > self.one_handed:
-                    if result_of_dice >= self.one_handed + 10:
-                        self.one_handed +=10
-                    else:
-                        self.athletics = dice
-            elif stat == "two_handed":
-                result_of_dice = dice(100)
-                if result_of_dice > self.two_handed:
-                    if result_of_dice >= self.two_handed + 10:
-                        self.two_handed += 10
-                    else:
-                        self.athletics = dice
-        else:
-            print("can't modify that value")
-
-def class Mastered(Skills):
-    def __init__(self, mastered: int):
-        self.mastered = mastered
-        super().__init__(skill_name=self)
-
-
-any = Skills(10,10,10,10)
-any.raise_skill("accounting")
-
-print(any.accounting)
+if __name__ == "__main__":
+    main()
